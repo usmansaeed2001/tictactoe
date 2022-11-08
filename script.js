b0a88eb2e991540966fed30daeaed9200857c6a4
usmansaeed2001 <muawan2001@gmail.com>
2022-11-08 18:47:06 +0500

    styling done

---
+++ script.js
@@ -0,0 +1,11 @@
+const cellElements = document.querySelectorAll('[data-cell]')
+
+
+cellElements.forEach(cell => {
+    cell.addEventListener('click', handleClick, { once: true })
+})
+
+
+function handleClick(e) {
+    console.log('clicked')
+}
